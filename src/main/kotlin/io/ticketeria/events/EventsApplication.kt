815f751dc482a52c25ee07df43a0655ffea228f5
Jessica de Lima Fileto <jessicalima@duck.com>
2024-08-31 14:19:25 -0300

    initial commit

---
+++ src/main/kotlin/io/ticketeria/events/EventsApplication.kt
@@ -0,0 +1,11 @@
+package io.ticketeria.events
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EventsApplication
+
+fun main(args: Array<String>) {
+	runApplication<EventsApplication>(*args)
+}
